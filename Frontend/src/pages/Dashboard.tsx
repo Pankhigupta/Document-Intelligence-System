7620c9b3e14c21ef94dd62aeea81d614ef6d5d91
Nivedika-sharma <sharmanivedika6@gmail.com>
2026-02-26 00:40:51 +0530

    finall

--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -415,7 +415,7 @@ export default function Dashboard() {
         </div>
       </div>
 
-      {/* ================= STATS CARDS ================= */}
+     
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-12">
 
         {/* Total Documents */}
